d0b07757838203cd5ff0b2d7c79ec2d1a23b2329
Querley Junio Rodrigues Ferreira <querleyjuniorodriguesferreira@gmail.com>
2026-03-19 13:09:30 -0300

    a bunch of minor changes.

--- GERENCIAMENTO_DE_BANCO_DE_DADOS/Atividade_06/query_versao_PSQL.sql
+++ GERENCIAMENTO_DE_BANCO_DE_DADOS/Atividade_06/query_versao_PSQL.sql
@@ -1,18 +1,16 @@
-create view vw_funcionario_info as
+with media_salarios as (
+    select 
+        t.title,
+        avg(s.salary) as media_salario
+    from titles t
+    join salaries s on t.emp_no = s.emp_no
+    where t.to_date > current_date
+    and s.to_date > current_date
+    group by t.title
+)
+
 select 
-    e.emp_no,
-    concat(e.first_name, ' ', e.last_name) as name,
-    d.dept_name,
-    t.title,
-    to_char(s.salary, 'FM999,999,999.00') as salary
-from employees e
-join dept_emp de on e.emp_no = de.emp_no
-join departments d on de.dept_no = d.dept_no
-join titles t on e.emp_no = t.emp_no
-join salaries s on e.emp_no = s.emp_no
-where de.to_date > current_date
-and t.to_date > current_date
-and s.to_date > current_date
-order by s.salary desc;
-
-
+    title,
+    media_salario
+from media_salarios
+order by media_salario desc;
