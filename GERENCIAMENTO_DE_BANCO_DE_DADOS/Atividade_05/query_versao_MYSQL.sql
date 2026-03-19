b36befffe2534f0a325e39db998d20e1cb52cf6d
Querley Junio Rodrigues Ferreira <querleyjuniorodriguesferreira@gmail.com>
2026-03-19 12:41:25 -0300

    added activity 5.

---
+++ GERENCIAMENTO_DE_BANCO_DE_DADOS/Atividade_05/query_versao_MYSQL.sql
@@ -0,0 +1,16 @@
+create view vw_funcionario_info as
+select 
+    e.emp_no,
+    concat(e.first_name, ' ', e.last_name) as name,
+    d.dept_name,
+    t.title,
+    format(s.salary, 2) as salary
+from employees e
+join dept_emp de on e.emp_no = de.emp_no
+join departments d on de.dept_no = d.dept_no
+join titles t on e.emp_no = t.emp_no
+join salaries s on e.emp_no = s.emp_no
+where de.to_date > current_date()
+and t.to_date > current_date()
+and s.to_date > current_date()
+order by s.salary desc;
